5bc20f426f96a1ebd88f33dd155d74cbdc3a9686
themdeee <mde819710@gmail.com>
2023-07-29 11:57:10 +0800

    Use ArduinoJson to parse the returned data

---
+++ src/esp_ddns.cpp
@@ -0,0 +1,224 @@
+#include "esp_ddns.h"
+
+String api_get_global_ipv4_address = "https://4.ipw.cn";
+String api_get_global_ipv6_address = "https://6.ipw.cn";
+String api_cloudflare_v4 = "https://api.cloudflare.com/client/v4";
+
+String get_global_ipv4_address(void)
+{
+    String global_ipv4_address = "";
+    
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        HTTPClient http;
+        
+        http.begin(api_get_global_ipv4_address.c_str());
+
+        uint16_t http_response_code = http.GET();
+        String response = http.getString();
+
+        http.end();
+
+        Serial.print("HTTP Response Code: ");
+        Serial.println(http_response_code);
+        Serial.print("Response: ");
+        Serial.println(response);
+
+        global_ipv4_address = response;
+    }
+    else
+    {
+        Serial.println("Wifi Disconnected");
+    }
+
+    return global_ipv4_address;
+}
+
+IPv6Address get_global_ipv6_address(void)
+{
+    esp_ip6_addr_t ipv6_address;
+    
+    esp_netif_t* get_esp_interface_netif(esp_interface_t interface);
+    
+    if(esp_netif_get_ip6_global(get_esp_interface_netif(ESP_IF_WIFI_STA), &ipv6_address))
+    {
+        return IPv6Address();
+    }
+    
+    return IPv6Address(ipv6_address.addr);
+}
+
+String get_root_domain(String sub_domain)
+{
+    String root_domain = "";
+    
+    std::smatch matches;
+    std::string domain(sub_domain.c_str());
+    std::regex pattern("^(?=^.{3,255}$)[a-zA-Z0-9][-a-zA-Z0-9]{0,61}[a-zA-Z0-9]\\.([a-zA-Z0-9][-a-zA-Z0-9]{0,61}[a-zA-Z0-9]\\.[a-zA-Z0-9][-a-zA-Z0-9]{0,61}[a-zA-Z0-9])$");
+
+    if (std::regex_search(domain, matches, pattern))
+    {
+        root_domain = String(matches[1].str().c_str());
+    }
+    else
+    {
+        Serial.println("Invalid sub domain");
+    }
+    
+    return root_domain;
+}
+
+String get_root_domain_id(String root_domain, String Authorization)
+{
+    String root_domain_id = "";
+    
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        HTTPClient http;
+        String api_cloudflare_get_root_domain_id = api_cloudflare_v4 + "/zones";
+        
+        http.begin(api_cloudflare_get_root_domain_id.c_str());
+        
+        http.addHeader("Content-Type", "application/json");
+        http.addHeader("Authorization", Authorization);
+
+        uint16_t http_response_code = http.GET();
+        String response = http.getString();
+
+        http.end();
+
+        Serial.print("HTTP Response Code: ");
+        Serial.println(http_response_code);
+        Serial.print("Response: ");
+        Serial.println(response);
+        
+        DynamicJsonDocument data(4096);
+        DeserializationError error = deserializeJson(data, response);
+
+        if (error)
+        {
+            Serial.print("Error on JSON parsing: ");
+            Serial.println(error.c_str());
+        }
+        else
+        {
+            if (data["success"])
+            {
+                JsonArray array = data["result"].as<JsonArray>();
+                
+                for (JsonObject obj : array)
+                {
+                    if (obj["name"].as<String>() == root_domain)
+                    {
+                        root_domain_id = obj["id"].as<String>();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    else
+    {
+        Serial.println("Wifi Disconnected");
+    }
+
+    return root_domain_id;
+}
+
+String get_sub_domain_id(String sub_domain, String Authorization)
+{
+    String sub_domain_id = "";
+
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        HTTPClient http;
+        String api_cloudflare_get_sub_domain_id = api_cloudflare_v4 + "/zones/" + get_root_domain_id(get_root_domain(sub_domain), Authorization) + "/dns_records";
+        
+        http.begin(api_cloudflare_get_sub_domain_id.c_str());
+        
+        http.addHeader("Content-Type", "application/json");
+        http.addHeader("Authorization", Authorization);
+        
+        uint16_t http_response_code = http.GET();
+        String response = http.getString();
+
+        http.end();
+
+        Serial.print("HTTP Response Code: ");
+        Serial.println(http_response_code);
+        Serial.print("Response: ");
+        Serial.println(response);
+
+        DynamicJsonDocument data(8192);
+        DeserializationError error = deserializeJson(data, response);
+
+        if (error)
+        {
+            Serial.print("Error on JSON parsing: ");
+            Serial.println(error.c_str());
+        }
+        else
+        {
+            if (data["success"])
+            {
+                JsonArray array = data["result"].as<JsonArray>();
+                
+                for (JsonObject obj : array)
+                {
+                    if (obj["name"].as<String>() == sub_domain)
+                    {
+                        sub_domain_id = obj["id"].as<String>();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    else
+    {
+        Serial.println("Wifi Disconnected");
+    }
+
+    return sub_domain_id;
+}
+
+void update_dns_record(String sub_domain, String Authorization, String ip_address, uint8_t version)
+{
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        HTTPClient http;
+        String api_cloudflare_update_dns_record = api_cloudflare_v4 + "/zones/" + get_root_domain_id(get_root_domain(sub_domain), Authorization) + "/dns_records/" + get_sub_domain_id(sub_domain, Authorization);
+        
+        http.begin(api_cloudflare_update_dns_record.c_str());
+        
+        http.addHeader("Content-Type", "application/json");
+        http.addHeader("Authorization", Authorization);
+        
+        String content = "";
+        
+        if (version == 4)
+        {
+            content = "{\"type\":\"A\",\"name\":\"" + sub_domain + "\",\"ttl\":1,\"proxied\":false,\"content\":\"" + ip_address + "\"}";
+        }
+        if (version == 6)
+        {
+            content = "{\"type\":\"AAAA\",\"name\":\"" + sub_domain + "\",\"ttl\":1,\"proxied\":false,\"content\":\"" + ip_address + "\"}";
+        }
+
+        Serial.println(content);
+
+        uint16_t http_response_code = http.PUT(content);
+        String response = http.getString();
+
+        http.end();
+
+        Serial.print("HTTP Response Code: ");
+        Serial.println(http_response_code);
+        Serial.print("Response: ");
+        Serial.println(response);
+    }
+    else
+    {
+        Serial.println("Wifi Disconnected");
+    }
+}
---
+++ src/esp_ddns.h
@@ -0,0 +1,17 @@
+#ifndef __ESP_DDNS_H
+#define __ESP_DDNS_H
+
+#include <regex>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+String get_global_ipv4_address(void);
+String get_root_domain(String sub_domain);
+String get_root_domain_id(String root_domain, String Authorization);
+String get_sub_domain_id(String sub_domain, String Authorization);
+
+IPv6Address get_global_ipv6_address(void);
+
+void update_dns_record(String sub_domain, String Authorization, String ip_address, uint8_t version);
+
+#endif
